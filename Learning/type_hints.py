06f3343ba8565f77f0426bdada2b460302e0f28f
ZoroOp007 <bkp825301@gmail.com>
2025-04-15 10:08:12 +0530

    first commit

---
+++ Learning/type_hints.py
@@ -0,0 +1,18 @@
+def getName(fullname : str, lastname : str):
+    fullname = fullname.title()
+    lastname = lastname.title()
+
+    return(fullname + lastname)
+
+def addition(a : str, b : int):
+
+    result = a+b
+
+    return result
+
+print(addition('1','4'))
+
+
+
+
+
